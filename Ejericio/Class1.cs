e8ac50da92cf59c44e4545e594654f16d2f999b1
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2024-02-02 10:47:21 -0600

    Add files via upload

---
+++ Ejericio/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using ConsoleApp2;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Pdiagno
+    {
+        public static void Main(string[] args)
+        {
+            Conver c = new Conver();
+            int opc;
+            double b, v;
+
+            c.Menu();
+
+            do
+            {
+                opc = c.validar("Seleccione");
+
+            } while (opc <= 0 || opc > 2);
+
+            switch (opc)
+            {
+                case 1:
+                    {
+                        c.Menu1();
+
+                        do
+                        {
+                            opc = c.validar("Seleccione");
+                        }while (opc <= 0 || opc > 2);
+
+                        switch (opc)
+                        {
+
+                            case 1:
+                                {
+                                    do
+                                    {
+                                        b = c.validar("Digite la cantidad de dolar a comprar: ");
+                                    }while(b <= 0 || b > 9000);
+                                    double dev = b * 36.99;
+                                    Console.WriteLine("El total requerido en cordobas es:" + dev);
+                            break;
+                                }
+
+                            case 2:
+                                {
+                                    do
+                                    {
+                                        v = c.validar("Digite la cantidad de euros a comprar: ");
+                                    } while (v <= 0 || v > 9000);
+                                    double ven = v * 1.1360;
+                                    Console.WriteLine("El total requerido en cordobas es: " + ven);
+                            break;
+                                }
+                        }
+                break;
+                    }
+
+                case 2:
+                    {
+                        c.Menu1();
+
+                        do
+                        {
+                            opc = c.validar("Seleccione");
+                        } while (opc <= 0 || opc > 2);
+
+                        switch (opc)
+                        {
+
+                            case 1:
+                                {
+                                    do
+                                    {
+                                        b = c.validar("Digite la cantidad de dolar a vender: ");
+                                    } while (b <= 0 || b > 9000);
+                                    double dev = b * 36.10;
+                                    Console.WriteLine("El total requerido en cordobas es:" + dev);
+                                    break;
+                                }
+
+                            case 2:
+                                {
+                                    do
+                                    {
+                                        v = c.validar("Digite la cantidad de euros a vender: ");
+                                    } while (v <= 0 || v > 9000);
+                                    double ven = v * 1.1360;
+                                    Console.WriteLine("El total recibido en cordobas es: " + ven);
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+
+            }
+        }
+
+    }
+}
---
+++ Ejericio/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Conver
+    {
+
+        double comp, vend;
+
+        public Conver()
+        {
+
+        }
+
+        public Conver(double comp)
+        {
+            this.comp = comp;
+        }
+
+        public void setComp(double comp)
+        {
+            this.comp=comp;
+        }
+
+        public void setVend(double vend)
+        {
+            this.vend = vend;   
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Que accion desea realizar? ");
+            Console.WriteLine("1-Compar. 2-Vender");
+        }
+
+        public void Menu1()
+        {
+            Console.WriteLine("Que tipo de divisas desea elegir?");
+            Console.WriteLine("1-Dolar. 2-Euro");
+        }
+
+        public  int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.WriteLine(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras: ", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+        
+    }
+
+}
